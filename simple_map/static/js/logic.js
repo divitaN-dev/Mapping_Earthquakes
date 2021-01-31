018c239cbceba0e8d45024f60dd125d89f610544
divitaN <divitanarang@divitas-MacBook-Pro.local>
2021-01-31 18:30:32 -0500

    add all files for geojson branch

--- simple_map/static/js/logic.js
+++ simple_map/static/js/logic.js
@@ -1,9 +1,118 @@
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+//coordinates for LA
+//let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
+//coordinates in between LA and SFO
+// let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+
+// let cityData = cities;
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 6,
+        accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         accessToken: API_KEY
     });
-    
-streets.addTo(map);
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+  };
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+let airportData = "https://raw.githubusercontent.com/divitaN-dev/Mapping_Earthquakes/Mapping_GeoJson_Points/majorAirports.json";
+// Grabbing our GeoJSON data.
+
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+// L.circleMarker([34.0522, -118.2437],{
+//     radius: 30,
+//     color: 'black',
+//     fillColor: '#ffffa1'
+// }).addTo(map);
+
+// cityData.forEach(city => {
+//     L.circleMarker(city.location,{
+//         radius: city.population/100000,
+//         color: 'orange'
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// })
+
+//co-ordinates for multiple lines
+// let line = [
+//     [33.9416, -118.4085],
+//     [37.6213, -122.3790],
+//     [40.7899, -111.9791],
+//     [47.4502, -122.3088]
+// ];
+
+// L.polyline(line, {
+//     color: 'red'
+// }).addTo(map)
+
+// GeoJSON Practice -
+// Add GeoJSON data.
+
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+// L.geoJSON(sanFranAirport).addTo(map);
+
+// // Grabbing our GeoJSON data.
+// L.geoJson(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//     //console.log(feature);
+//       return L.marker(latlng)
+//       .bindPopup("<h2>" + feature.properties.name + "</h2>");
+//     }
+
+//   }).addTo(map);
+
+// L.geoJSON(sanFranAirport, {
+//     onEachFeature: function(feature, layer){
+//         console.log(layer);
+//         layer.bindPopup();
+//     }
+// }).addTo(map)
